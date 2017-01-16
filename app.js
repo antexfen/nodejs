33d52ef68777d5edeaf4e67526fd80be081ce87b
anteneh <anteneh.mengistu@mindtree.com>
2017-01-16 16:45:59 -0500

    edited git ignore file

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ app.get("/", function(req,res){
     res.send("<html><body><h1>You called root path</h1></body></html>")
 });
 
+app.get("/data",function(req,res){
+    res.send("you called data")
+
+});
+
+app.get("/download", function(req,res){
+
+    res.download("./package.json",function(){
+        console.log("download is over");
+    })
+
+});
 
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
